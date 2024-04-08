c5ad4c8642d07ac57fc5be8de6af85145f6ce8d4
Julian Schilliger <schillij@gmail.com>
2024-04-08 14:07:28 +0200

    working on multi gpu rendering

--- ThaumatoAnakalyptor/mesh_to_surface.py
+++ ThaumatoAnakalyptor/mesh_to_surface.py
@@ -102,7 +102,7 @@ class MyPredictionWriter(BasePredictionWriter):
                 indexes_3d = indexes_3d.cpu()
                 
             values = self.all_gather_nd(values)
-            indexes_3d = self.all_gather_nd(indexes_3d)
+            indexes_3d = self.all_gather_nd(indexes_3d, trainer)
             rank_pred_dict =(values, indexes_3d)
 
             if trainer.world_size == 1: # Single GPU
@@ -115,7 +115,7 @@ class MyPredictionWriter(BasePredictionWriter):
             print(e)
             return None
         
-    def all_gather_nd(self, tensor):
+    def all_gather_nd(self, tensor, trainer):
         """
         Gathers tensor arrays of different lengths in a list.
         The length dimension is 0. This supports any number of extra dimensions in the tensors.
@@ -127,9 +127,8 @@ class MyPredictionWriter(BasePredictionWriter):
         Returns:
             (Tensor): output list of tensors that can be of different sizes
         """
-        world_size = dist.get_world_size()
         local_size = torch.tensor(tensor.size(), device=tensor.device)
-        all_sizes = [torch.zeros_like(local_size) for _ in range(world_size)]
+        all_sizes = [torch.zeros_like(local_size) for _ in range(trainer.world_size)]
         dist.all_gather(all_sizes, local_size)
 
         max_length = max(size[0] for size in all_sizes)
@@ -140,7 +139,7 @@ class MyPredictionWriter(BasePredictionWriter):
             padding = torch.zeros(pad_size, device=tensor.device, dtype=tensor.dtype)
             tensor = torch.cat((tensor, padding))
 
-        all_tensors_padded = [torch.zeros_like(tensor) for _ in range(world_size)]
+        all_tensors_padded = [torch.zeros_like(tensor) for _ in range(trainer.world_size)]
         dist.all_gather(all_tensors_padded, tensor)
         all_tensors = []
         for tensor_, size in zip(all_tensors_padded, all_sizes):
