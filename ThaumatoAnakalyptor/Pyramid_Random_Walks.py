65e9471d5794ac1cf1e4fc8d0e4e076698fa764f
Julian <scjulian@student.ethz.ch>
2024-07-04 15:21:30 +0200

    experiment settings

--- ThaumatoAnakalyptor/Pyramid_Random_Walks.py
+++ ThaumatoAnakalyptor/Pyramid_Random_Walks.py
@@ -1819,7 +1819,7 @@ class RandomWalkSolver:
         return contracted_graph
 
     # def solve_pyramid(self, path, pyramid_up_nr_average=1000, max_nr_walks=100, nr_walks_per_node=100, max_unchanged_walks=10000, max_steps=100, max_tries=6, min_steps=10, min_end_steps=4, l=7, l_subsequent=6, n=4, stop_event=None):
-    def solve_pyramid(self, path, pyramid_up_nr_average=1000, max_nr_walks=100, nr_walks_per_node=100, max_unchanged_walks=10000, max_steps=100, max_tries=6, min_steps=10, min_end_steps=4, l=5, l_subsequent=4, n=6, stop_event=None):
+    def solve_pyramid(self, path, pyramid_up_nr_average=1000, max_nr_walks=100, nr_walks_per_node=100, max_unchanged_walks=10000, max_steps=100, max_tries=6, min_steps=10, min_end_steps=4, l=7, l_subsequent=6, n=4, stop_event=None):
         # pyramid solution utilizing random walks to deduct the nodes connections
         # samples landmarks and computing their connection certainties iteratively while contracting the graph with the help of the landmarks
         # when reaching the pyramid top, the graph is expanded again with the help of the landmarks. the top pyramid node is the starting node.
@@ -1851,8 +1851,8 @@ class RandomWalkSolver:
                         assert len(landmark_nodes) > 0, "No landmark nodes sampled."
                         print(f"Pyramid Index is {pyramid_index}. Remaining Landmark Nodes: {len(landmark_nodes)}")
                         # compute landmark nodes connections
-                        max_steps_ = max_steps if len(graphs) == 1 else max_steps - 1
-                        min_steps_ = min_steps if len(graphs) == 1 else min_steps - 1
+                        max_steps_ = max_steps
+                        min_steps_ = 10 if len(graphs) == 1 else 9
                         pyramid_up_nr_average_ = pyramid_up_nr_average if len(graphs) == 1 else pyramid_up_nr_average * 2
                         # aggregated_connections = self.solve_pyramid_up(graph, landmark_nodes, max_nr_walks=pyramid_up_nr_average_, max_steps=max_steps_, max_tries=max_tries, min_steps=min_steps_, stop_event=stop_event)
                         aggregated_connections = self.solve_pyramid_up_cpp(graph, landmark_nodes, max_nr_walks=pyramid_up_nr_average_, max_steps=max_steps_, max_tries=max_tries, min_steps=min_steps_, stop_event=stop_event)
@@ -2685,6 +2685,9 @@ def compute(overlapp_threshold, start_point, path, recompute=False, compute_cpp_
             scroll_graph = load_graph(recompute_path)
     scroll_graph.set_overlapp_threshold(overlapp_threshold)
     scroll_graph.start_block, scroll_graph.patch_id = start_block, patch_id
+    
+    scroll_graph.flip_winding_direction()
+    scroll_graph.save_graph(recompute_path)
 
     print(f"Number of nodes in the graph: {len(scroll_graph.nodes)}")
 
