84182d94e2894ac2db7b48bceaf9013e255da820
Julian <scjulian@student.ethz.ch>
2024-07-05 18:39:06 +0200

    experiment settings

--- ThaumatoAnakalyptor/sheet_generation/solver.cpp
+++ ThaumatoAnakalyptor/sheet_generation/solver.cpp
@@ -596,7 +596,12 @@ std::tuple<std::vector<NodePtr>, std::vector<K>, std::vector<int>> pick_start_no
     for (int i = 0; i < nr_walks; ++i) {
         int p = distrib(gen) % 100;
         if (p < 15) {
-            int rand_index = std::abs(distrib(gen) % landmark_nodes.size());
+            int rand_index = distrib(gen) % landmark_nodes.size();
+            if (rand_index < 0)
+            {
+                rand_index = - rand_index;
+            }
+            
             NodePtr node = landmark_nodes[rand_index];
             K k = landmark_ks[rand_index];
 
