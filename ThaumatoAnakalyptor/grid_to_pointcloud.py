PointCloud gradient fix

ThaumatoAnakalyptor/grid_to_pointcloud.py
@@ -85,9 +85,13 @@ def load_grid(path_template, cords, grid_block_size=500, cell_block_size=500, ui
                     # print(f"File {path} does not exist.")
                     continue
 
-                # Read the image
-                with tifffile.TiffFile(path) as tif:
-                    images = tif.asarray()
+                try:
+                    # Read the image
+                    with tifffile.TiffFile(path) as tif:
+                        images = tif.asarray()
+                except Exception as e:
+                    print(f"Error loading {path}: {e}")
+                    continue
 
                 if uint8:
                     images = np.uint8(images//256)
@@ -519,14 +523,24 @@ class GridDataset(Dataset):
     def get_writer(self):
         return self.writer
     
+    # def get_reference_vector(self, corner_coords):
+    #     block_point = np.array(corner_coords) + self.grid_block_size//2 # y, x, z
+    #     umbilicus_point = umbilicus_xz_at_y(self.umbilicus_points, block_point[2]) # x, z, y
+    #     umbilicus_point = umbilicus_point[[0, 2, 1]] # ply to corner coords # x, y, z
+    #     umbilicus_normal = block_point - umbilicus_point 
+    #     # umbilicus_normal = umbilicus_normal[[2, 0, 1]] # corner coords to tif 
+    #     umbilicus_normal = umbilicus_normal[[2, 1, 0]] # corner coords to tif 
+    #     unit_umbilicus_normal = umbilicus_normal / np.linalg.norm(umbilicus_normal)
+    #     return unit_umbilicus_normal # y, z, x
+
     def get_reference_vector(self, corner_coords):
-        block_point = np.array(corner_coords) + self.grid_block_size//2
-        umbilicus_point = umbilicus_xz_at_y(self.umbilicus_points, block_point[2])
-        umbilicus_point = umbilicus_point[[0, 2, 1]] # ply to corner coords
-        umbilicus_normal = block_point - umbilicus_point
-        umbilicus_normal = umbilicus_normal[[2, 0, 1]] # corner coords to tif
+        block_point = np.array(corner_coords) + self.grid_block_size//2 # y, x, z
+        block_point = block_point[[0, 2, 1]] # y, z, x
+        umbilicus_point = umbilicus_xz_at_y(self.umbilicus_points, block_point[1]) # y, z, x
+        umbilicus_normal = (block_point - umbilicus_point).astype(np.float32)
         unit_umbilicus_normal = umbilicus_normal / np.linalg.norm(umbilicus_normal)
-        return unit_umbilicus_normal
+        unit_umbilicus_normal = unit_umbilicus_normal[[2, 1, 0]]
+        return unit_umbilicus_normal.astype(np.float32) # y, z, x
     
     def __len__(self):
         return len(self.blocks_to_process)
@@ -542,7 +556,7 @@ class GridDataset(Dataset):
         
         # Convert NumPy arrays to PyTorch tensors
         block_tensor = torch.from_numpy(block).float()  # Convert to float32 tensor
-        reference_vector_tensor = torch.from_numpy(reference_vector).float()
+        reference_vector_tensor = torch.from_numpy(reference_vector).to(dtype=torch.float16)
         
         return block_tensor, reference_vector_tensor, corner_coords, self.grid_block_size, padding
     
@@ -580,7 +594,7 @@ class PointCloudModel(pl.LightningModule):
         for grid_volume, reference_vector, corner_coords, grid_block_size, padding in zip(grid_volumes, reference_vectors, corner_coordss, grid_block_sizes, paddings):
             # Perform surface detection for this block block
             recto_tensor_tuple, verso_tensor_tuple = surface_detection(grid_volume, reference_vector, blur_size=11, window_size=9, stride=1, threshold_der=0.075, threshold_der2=0.002, convert_to_numpy=False)
-            points_r_tensor, normals_r_tensor = recto_tensor_tuple
+            points_r_tensor, normals_r_tensor = recto_tensor_tuple # z, y, x
             points_v_tensor, normals_v_tensor = verso_tensor_tuple
             # Extract actual volume size from the oversized input block
             points_r_tensor, normals_r_tensor = extract_size_tensor(points_r_tensor, normals_r_tensor, padding, grid_block_size+padding) # 0, 0, 0 is the minimum corner of the grid block
@@ -589,7 +603,7 @@ class PointCloudModel(pl.LightningModule):
             ### Adjust the 3D coordinates of the points based on their position in the larger volume
 
             # permute the axes to match the original volume
-            points_r_tensor = points_r_tensor[:, [1, 0, 2]]
+            points_r_tensor = points_r_tensor[:, [1, 0, 2]] # y, z, x
             normals_r_tensor = normals_r_tensor[:, [1, 0, 2]]
             points_v_tensor = points_v_tensor[:, [1, 0, 2]]
             normals_v_tensor = normals_v_tensor[:, [1, 0, 2]]
@@ -639,18 +653,18 @@ def compute(disk_load_save, base_path, volume_subpath, pointcloud_subpath, maxim
     pointcloud_subpath_recto = pointcloud_subpath + "_recto"
     pointcloud_subpath_verso = pointcloud_subpath + "_verso"
     pointcloud_subpath_colorized = pointcloud_subpath + "_colorized"
-    src_dir = base_path + "/" + volume_subpath + "/"
+    src_dir = os.path.join(base_path, volume_subpath)
     dest_dir_r = src_dir.replace(volume_subpath, pointcloud_subpath_recto).replace(disk_load_save[0], disk_load_save[1])
     dest_dir_v = src_dir.replace(volume_subpath, pointcloud_subpath_verso).replace(disk_load_save[0], disk_load_save[1])
 
     # Cleanup temp files
     cleanup_temp_files(dest_dir_r)
     cleanup_temp_files(dest_dir_v)
-    path_template = src_dir + "cell_yxz_{:03}_{:03}_{:03}.tif"
+    path_template = os.path.join(src_dir, "cell_yxz_{:03}_{:03}_{:03}.tif")
     save_template_r = path_template.replace(".tif", ".ply").replace(volume_subpath, pointcloud_subpath_recto).replace(disk_load_save[0], disk_load_save[1])
     save_template_v = path_template.replace(".tif", ".ply").replace(volume_subpath, pointcloud_subpath_verso).replace(disk_load_save[0], disk_load_save[1])
 
-    umbilicus_path = src_dir + "umbilicus.txt"
+    umbilicus_path = os.path.join(src_dir, "umbilicus.txt")
     save_umbilicus_path = umbilicus_path.replace(".txt", ".ply").replace(disk_load_save[0], disk_load_save[1])
     save_umbilicus_path = save_umbilicus_path.replace(volume_subpath, pointcloud_subpath)
 
@@ -672,18 +686,19 @@ def compute(disk_load_save, base_path, volume_subpath, pointcloud_subpath, maxim
 
     if fix_umbilicus:
         # Load old umbilicus
-        umbilicus_path_old = src_dir + "umbilicus_old.txt"
+        umbilicus_path_old = os.path.join(src_dir, "umbilicus_old.txt")
         # Usage
         umbilicus_raw_points_old = load_xyz_from_file(umbilicus_path_old)
         umbilicus_points_old = umbilicus(umbilicus_raw_points_old)
     else:
         umbilicus_points_old = None
+        umbilicus_raw_points_old = None
 
     # Starting grid block at corner (3000, 4000, 2000) to match cell_yxz_006_008_004
     # (2600, 2200, 5000)
     if not skip_surface_blocks:
         # compute_surface_for_block_multiprocessing(start_block, pointcloud_base, path_template, save_template_v, save_template_r, umbilicus_points, grid_block_size=200, recompute=recompute, fix_umbilicus=fix_umbilicus, umbilicus_points_old=umbilicus_points_old, maximum_distance=maximum_distance)
-        grid_inference(pointcloud_base, start_block, path_template, save_template_v, save_template_r, umbilicus_points, umbilicus_points_old, grid_block_size=200, recompute=recompute, fix_umbilicus=fix_umbilicus, maximum_distance=maximum_distance, batch_size=1*int(CFG['GPUs']))
+        grid_inference(pointcloud_base, start_block, path_template, save_template_v, save_template_r, umbilicus_raw_points, umbilicus_raw_points_old, grid_block_size=200, recompute=recompute, fix_umbilicus=fix_umbilicus, maximum_distance=maximum_distance, batch_size=1*int(CFG['GPUs']))
     else:
         print("Skipping surface block computation.")
 

ThaumatoAnakalyptor/surface_detection.py
@@ -21,8 +21,8 @@ def sobel_filter_3d(input, chunks=4, overlap=3, device=None):
          [[ 1, 0, -1], [ 2, 0, -2], [ 1, 0, -1]]],
     ], dtype=torch.float16).to(device)
 
-    sobel_y = sobel_x.transpose(2, 3)
-    sobel_z = sobel_x.transpose(1, 3)
+    sobel_y = sobel_x.transpose(1, 3)
+    sobel_z = sobel_x.transpose(2, 3)
 
     # Add an extra dimension for the input channels
     sobel_x = sobel_x[None, ...]
@@ -66,7 +66,7 @@ def sobel_filter_3d(input, chunks=4, overlap=3, device=None):
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
 
-    return vectors.squeeze(0).squeeze(0).to(device)
+    return vectors.squeeze(0).squeeze(0).to(device, dtype=torch.float16)
 
 ## own code
 
@@ -95,43 +95,50 @@ def uniform_blur3d(channels=1, size=3, device=None):
 
 # Function to normalize vectors to unit length
 def normalize(vectors):
-    return vectors / vectors.norm(dim=-1, keepdim=True)
+    # Find 0 vectors and make them 1, 0, 0
+    # vectors_norm = vectors.norm(dim=-1, keepdim=True).expand(vectors.shape)
+    # zero_vectors = vectors_norm == 0
+    # vectors[zero_vectors] = torch.tensor([1.0, 0.0, 0.0], device=vectors.device, dtype=vectors.dtype)
+
+    return vectors / vectors.norm(dim=-1, keepdim=True).expand(vectors.shape)
 
 # Function to calculate angular distance between vectors
 def angular_distance(v1, v2):
     v1 = v1.unsqueeze(0) if v1.dim() == 1 else v1
     v2 = v2.unsqueeze(0) if v2.dim() == 1 else v2
-    return torch.acos(torch.clamp((v1 * v2).sum(-1), -1.0, 1.0))
+    dot_v1_v2 = (v1 * v2).sum(-1)
+    # print(f"Dot product: {dot_v1_v2}")
+    return torch.acos(torch.clamp(dot_v1_v2, -1.0, 1.0))
 
 # Mean indiscriminative Loss function for a batch of candidate vectors and a batch of input vectors
 def loss(candidates, vectors):
-    vector_normed = normalize(vectors)
-    pos_distance = angular_distance(candidates[:, None, :], vector_normed[None, :, :])
-    neg_distance = angular_distance(-candidates[:, None, :], vector_normed[None, :, :])
+    # Prepare for pairwise loss
+    candidates_expanded = candidates.unsqueeze(1).expand(-1, vectors.shape[0], -1)
+    vectors_expanded = vectors.unsqueeze(0).expand(candidates.shape[0], -1, -1)
+    # pairwise angulare distance
+    pos_distance = torch.abs(angular_distance(candidates_expanded, vectors_expanded))
+    neg_distance = torch.abs(angular_distance(-candidates_expanded, vectors_expanded))
+    # Find the minimum loss for each candidate-vector pair (flipping the candidate vector)
     losses = torch.min(torch.stack((pos_distance, neg_distance), dim=-1), dim=-1)[0]
-    
-    # calculate the norm of the vectors and use it to scale the losses
-    vector_norms = torch.norm(vectors, dim=-1)
-    scaled_losses = losses * vector_norms
-    
-    return scaled_losses
+    # Reduce to get total loss per candidate
+    summed_loss = losses.sum(dim=-1)    
+    return summed_loss
 
 # Function to find the vector that is the propper mean vector for the input vectors vs when -v = v
 def find_mean_indiscriminative_vector(vectors, n, device):
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # Normalize the input vectors
-    vectors = normalize(vectors)
-    
     # Generate n random unit vectors
     random_vectors = torch.randn(n, 3, device=device)
+    # Normalize the input vectors
     random_vectors = normalize(random_vectors)
+    vectors = normalize(vectors)    
 
     # Compute the total loss for each candidate
-    total_loss = loss(random_vectors, vectors).sum(dim=-1)
+    total_loss = loss(random_vectors, vectors)
+    # Pick the candidate with the smallest loss over all vectors
+    argmin_loss = torch.argmin(total_loss)
 
     # Find the best candidate
-    best_vector = random_vectors[torch.argmin(total_loss)]
-
+    best_vector = random_vectors[argmin_loss]
     return best_vector
 
 # Function to adjust the amplitude of the mean vector to the mean amplitude of the input vectors
@@ -142,7 +149,7 @@ def adjust_mean_vector_amplitude(mean_vector, vectors):
 
 # Function that  projects vector a onto vector b
 def vector_projection(a, b):
-    return (a * b).sum(-1, keepdim=True) * b / b.norm(dim=-1, keepdim=True)**2
+    return (a * b).sum(-1, keepdim=True).expand(b.shape) * b / b.norm(dim=-1, keepdim=True).expand(b.shape)**2
 
 # Function that adjusts the norm of vector a to the norm of vector b based on their direction
 def adjusted_norm(a, b):
@@ -150,13 +157,14 @@ def adjusted_norm(a, b):
     projection = vector_projection(a, b)
     
     # Compute the dot product of the projected vector and the original vector b
-    dot_product = (projection * b).sum(-1, keepdim=True)
+    dot_product = (projection * b).sum(-1)
     
     # Compute the norm of the projection
     projection_norm = projection.norm(dim=-1)
     
     # Adjust the norm based on the sign of the dot product
-    adjusted_norm = torch.sign(dot_product.squeeze()) * projection_norm
+    adjusted_norm = torch.sign(dot_product) * projection_norm
+    # print(f"shape adjusted_norm: {adjusted_norm.shape}")
     
     return adjusted_norm
 
@@ -171,9 +179,10 @@ def scale_to_0_1(tensor):
     tensor_min = torch.min(clipped_tensor)
     tensor_max = torch.max(clipped_tensor)
     tensor_scale = torch.max(torch.abs(tensor_min), torch.abs(tensor_max))
+    if tensor_scale < 1e-8:
+        tensor_scale = 1e-8
     scaled_tensor = clipped_tensor / tensor_scale
     return scaled_tensor
-
 
 # Function that convolutes a 3D Volume of vectors to find their mean indiscriminative vector
 def vector_convolution(input_tensor, window_size=20, stride=20, device=None):
@@ -195,9 +204,16 @@ def vector_convolution(input_tensor, window_size=20, stride=20, device=None):
                 # extract the vectors within the window
                 window_vectors = input_tensor[i:i+window_size, j:j+window_size, k:k+window_size]
                 window_vectors = window_vectors.reshape(-1, 3)  # flatten the 3D window into a 2D tensor
+
+                all_zeros = torch.all(window_vectors == 0)
+                if not all_zeros:
+                    window_vectors_mask = torch.any(window_vectors != 0, dim=-1)
+                    window_vectors = window_vectors[window_vectors_mask]
                 
-                # calculate the closest vector
-                best_vector = find_mean_indiscriminative_vector(window_vectors, 100, device)  # adjust the second parameter as needed
+                    # calculate the closest vector
+                    best_vector = find_mean_indiscriminative_vector(window_vectors, 100, device)  # adjust the second parameter as needed
+                else:
+                    best_vector = torch.tensor([1.0, 0.0, 0.0], device=device)
                 # check if the indices are within the output_tensor's dimension
                 if i//stride < output_tensor.shape[0] and j//stride < output_tensor.shape[1] and k//stride < output_tensor.shape[2]:
                     # store the result in the output tensor
@@ -214,10 +230,14 @@ def interpolate_to_original(input_tensor, output_tensor):
     input_tensor = input_tensor.permute(3, 0, 1, 2)
 
     # Use 3D interpolation to resize output_tensor to match the shape of input_tensor.
-    interpolated_tensor = F.interpolate(output_tensor.unsqueeze(0), size=input_tensor.shape[1:], mode='trilinear', align_corners=False)
+    interpolated_tensor = F.interpolate(output_tensor.unsqueeze(0), size=input_tensor.shape[1:], mode='trilinear', align_corners=True).squeeze(0) # add and remove one dimension for batch, channels, spatial 1, spatial 2, spatial 3
+
 
     # Return the tensor to its original shape.
-    interpolated_tensor = interpolated_tensor.squeeze(0).permute(1, 2, 3, 0)
+    interpolated_tensor = interpolated_tensor.permute(1, 2, 3, 0)
+
+    # Normalize the interpolated tensor
+    interpolated_tensor = normalize(interpolated_tensor)
 
     return interpolated_tensor
 
@@ -225,6 +245,7 @@ def interpolate_to_original(input_tensor, output_tensor):
 def adjust_vectors_to_global_direction(input_tensor, global_reference_vector):
     # Compute dot product of each vector in the input tensor with the global reference vector.
     # The resulting tensor will have the same shape as the input tensor, but with the last dimension squeezed out.
+    global_reference_vector = global_reference_vector.unsqueeze(0).unsqueeze(0).unsqueeze(0).expand(input_tensor.shape)
     dot_products = (input_tensor * global_reference_vector).sum(dim=-1, keepdim=True)
     # Create a mask of the same shape as the dot products tensor, 
     # with True wherever the dot product is negative.
@@ -232,6 +253,7 @@ def adjust_vectors_to_global_direction(input_tensor, global_reference_vector):
     
     # Expand the mask to have the same shape as the input tensor
     mask = mask.expand(input_tensor.shape)
+    # print(f"Input tensor shape: {input_tensor.shape}, dot products shape: {dot_products.shape}, mask shape: {mask.shape}")
 
     # Negate all vectors in the input tensor where the mask is True.
     adjusted_tensor = input_tensor.clone()
@@ -272,7 +294,6 @@ def surface_detection(volume, global_reference_vector, blur_size=3, sobel_chunks
 
     # Project the Sobel result onto the adjusted vectors and calculate the norm
     first_derivative = adjusted_norm(sobel_vectors, adjusted_vectors_interp)
-    fshape = first_derivative.shape
     
     first_derivative = scale_to_0_1(first_derivative)
 
@@ -288,10 +309,7 @@ def surface_detection(volume, global_reference_vector, blur_size=3, sobel_chunks
     # Check where the second derivative is zero and the first derivative is above a threshold
     points_to_mark = torch.where(mask)
 
-    # Subsample the points to mark
-    #subsample_nr = 2000000
     coords = torch.stack(points_to_mark, dim=1)
-    #coords = subsample_uniform(coords, subsample_nr)
     
     # Cluster the surface points
     coords_normals = adjusted_vectors_interp[coords[:, 0], coords[:, 1], coords[:, 2]]