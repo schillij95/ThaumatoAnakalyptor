bcd4f0c45d01d09714912411412dbbbc570825a9
Julian Schilliger <schillij@gmail.com>
2024-07-27 20:27:32 +0200

    Added FAS solver

--- FAS/sequence_to_graph.py
+++ FAS/sequence_to_graph.py
@@ -31,6 +31,19 @@ class Graph:
         # set length of nodes and edges
         self.nodes_length = len(self.nodes)
         self.edges_length = len(self.edges)
+
+    def compute_node_edges(self, verbose=True):
+        """
+        Compute the edges for each node and store them in the node dictionary.
+        """
+        if verbose:
+            print("Computing node edges...")
+        for node in tqdm(self.nodes, desc="Adding nodes") if verbose else self.nodes:
+            self.nodes[node]['edges'] = []
+        for edge in tqdm(self.edges, desc="Computing node edges") if verbose else self.edges:
+            for k in self.edges[edge]:
+                self.nodes[edge[0]]['edges'].append(edge)
+                self.nodes[edge[1]]['edges'].append(edge)
     
     def update_winding_angles(self, nodes, ks, update_winding_angles=False):
         nodes = [tuple(int(node[i]) for i in range(4)) for node in nodes]
@@ -128,38 +141,51 @@ class ScrollGraph(Graph):
         print(f"Found largest connected component with {len(result)} nodes.")
         return result
     
-    def bfs_ks(self):
+    def bfs_ks(self, start_node):
         """
         Breadth-first search from start_node.
         """
-        sn = list(self.nodes.keys())[0]
-        start_node = tuple(int(sn[i]) for i in range(4))
+        nodes_k_dict = {}
         nodes = []
         ks = []
-        visited = set()
+        min_k = None
+        max_k = None
         queue = [(start_node, 0)]
+        tqdm_object = tqdm(total=100*len(self.nodes))
         while queue:
             node, k = queue.pop(0)
-            if node not in visited:
-                visited.add(node)
-                nodes.append(node)
-                ks.append(k)
+            if min_k is None or k < min_k:
+                min_k = k
+            if max_k is None or k > max_k:
+                max_k = k
+            # tqdm_object.update(1)
+            print(len(nodes_k_dict), min_k, max_k)
+            if node not in nodes_k_dict or (nodes_k_dict[node] <= 0 and k < nodes_k_dict[node] or (nodes_k_dict[node] >= 0 and k > nodes_k_dict[node])):
+                nodes_k_dict[node] = k
                 edges = self.nodes[node]['edges']
                 for edge in edges:
+                    flip_edge = edge[0] != node
                     try:
                         for k_delta in self.edges[edge]:
                             if self.edges[edge][k_delta]['bad_edge']:
                                 continue
                             if self.edges[edge][k_delta]['same_block']:
                                 continue
-                            queue.append((edge[0] if edge[0] != node else edge[1], k + k_delta))
+                            k_delta_flip = -k_delta if flip_edge else k_delta
+                            queue.append((edge[0] if flip_edge else edge[1], k + k_delta_flip))
                     except:
                         continue
+        tqdm_object.close()
+        for node in nodes_k_dict:
+            k = nodes_k_dict[node]
+            nodes.append(node)
+            ks.append(k)
         return nodes, ks
 
 def load_thaumato_graph(graph_path):
     with open(graph_path, 'rb') as file:
         thaumato_graph = pickle.load(file)
+    thaumato_graph.remove_nodes_edges([list(thaumato_graph.nodes)[-1]]) # indexing error somewhere, last node is missing after ArrayFAS
     # Convert to "standart graph format". nodes numbered from 0 to n-1, edges are stored in a dictionary
     graph_dict = {}
     translation_dict = {}
@@ -200,7 +226,49 @@ def load_sequence(sequence_path):
     sequence_dict = {}
     for i, node in enumerate(sequence):
         sequence_dict[node] = i
-    return sequence_dict
+    return sequence, sequence_dict
+
+def calculate_ks(sequence, sequence_dict, translation_dict, thaumato_graph):
+    visited_nodes = [False for _ in range(len(sequence))]
+    ks = [0 for _ in range(len(sequence))]
+    nodes_names = list(thaumato_graph.nodes)
+
+    nr_fresh_starts = 0
+    for i in range(len(sequence)):
+        if not visited_nodes[i]:
+            nr_fresh_starts += 1
+            visited_nodes[i] = True
+            print(f"Fresh start at {i}, sum of visited nodes: {sum(visited_nodes)}")
+            for i, node in enumerate(sequence):
+                if not visited_nodes[i]:
+                    continue
+                try:
+                    node_name = nodes_names[node]
+                except:
+                    print(f"Node not in graph {node}")
+                node_graph = thaumato_graph.nodes[node_name]
+                edges = node_graph['edges']
+                for edge in edges:
+                    flip_edge = edge[0] != node_name
+                    next_node_name = edge[0] if flip_edge else edge[1]
+                    next_node = translation_dict[next_node_name]
+                    next_node_sequence_position = sequence_dict[next_node]
+                    if next_node_sequence_position < i:
+                        continue
+                    for k in thaumato_graph.edges[edge]:
+                        if thaumato_graph.edges[edge][k]['bad_edge']:
+                            continue
+                        if thaumato_graph.edges[edge][k]['same_block']:
+                            continue
+                        k_flip = -k if flip_edge else k
+                        if k_flip < 0:
+                            print(f"Negative k: {k_flip}")
+                        else:
+                            ks[next_node_sequence_position] = ks[i] + k_flip
+                            visited_nodes[next_node_sequence_position] = True
+    print(f"{nr_fresh_starts} fresh starts to Calculated ks, min: {min(ks)}, max: {max(ks)}, nr of visited nodes: {sum(visited_nodes)}")
+    return ks
+                
 
 if __name__ == "__main__":
     # Argparse
@@ -212,8 +280,9 @@ if __name__ == "__main__":
     # Load the graph
     thaumato_graph, graph_dict, translation_dict = load_thaumato_graph(args.graph)
     # Load the sequence
-    sequence_dict = load_sequence(args.graph_name+"_sequence.txt")
-    
+    sequence, sequence_dict = load_sequence(args.graph_name+"_sequence.txt")
+    calculate_ks(sequence, sequence_dict, translation_dict, thaumato_graph)
+    exit()
     # Delete Bad nodes
     edges = thaumato_graph.edges
     deletion_edges = []
@@ -224,6 +293,7 @@ if __name__ == "__main__":
             seq_i0 = sequence_dict[i0]
             seq_i1 = sequence_dict[i1]
         except:
+            print(f"Node not in sequence {i0} or {i1}")
             deletion_edges.append(edge)
             continue
         winding_angle0 = thaumato_graph.nodes[edge[0]]['winding_angle']
@@ -237,31 +307,48 @@ if __name__ == "__main__":
             if edges[edge][k]['same_block']:
                 deletion_ks.append(k)
                 continue
-            if k < 0 and seq_i1 > seq_i0:
+            if k < 0 and seq_i1 >= seq_i0:
                 deletion_ks.append(k)
             elif k == 0:
-                if winding_angle0 < winding_angle1 and seq_i0 > seq_i1:
+                if winding_angle0 <= winding_angle1 and seq_i0 >= seq_i1:
                     deletion_ks.append(k)
-                elif winding_angle0 >= winding_angle1 and seq_i0 < seq_i1:
+                elif winding_angle1 <= winding_angle0 and seq_i1 >= seq_i0:
                     deletion_ks.append(k)
-            elif k > 0 and seq_i0 < seq_i1:
+            elif k > 0 and seq_i0 >= seq_i1:
                 deletion_ks.append(k)
         for k in deletion_ks:
             del thaumato_graph.edges[edge][k]
             if len(thaumato_graph.edges[edge]) == 0:
+                print(f"Deleting edge {edge}")
                 deletion_edges.append(edge)
     for edge in deletion_edges:
         del thaumato_graph.edges[edge]
+        for n in edge:
+            if n in thaumato_graph.nodes:
+                thaumato_graph.nodes[n]['edges'].remove(edge)
     
     print(f"Deleted bad edges")
     print(f"Remaining edges: {len(thaumato_graph.edges)}")
+    
+    thaumato_graph.compute_node_edges()
 
     # Prune the graph
     thaumato_graph.largest_connected_component(delete_nodes=True)
+    thaumato_graph.compute_node_edges()
     print(f"Remaining nodes: {len(thaumato_graph.nodes)}")
 
+
     # Update winding angles
-    nodes, ks = thaumato_graph.bfs_ks()
+    start_node = None
+    seq_start_node = len(thaumato_graph.nodes)
+    for node in thaumato_graph.nodes:
+        i0 = translation_dict[tuple(node)]
+        seq_i0 = sequence_dict[i0]
+        if start_node is None or seq_i0 < seq_start_node:
+            start_node = tuple(node)
+            seq_start_node = seq_i0
+
+    nodes, ks = thaumato_graph.bfs_ks(start_node, translation_dict, sequence_dict)
     thaumato_graph.update_winding_angles(nodes, ks, update_winding_angles=True)
 
     # Save the graph
